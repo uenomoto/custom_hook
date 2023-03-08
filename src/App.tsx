1e0e58435ae27067b3ac2571ac3a91528922840a
上野元希 <uenomotoki@uenomotomarenoMacBook-Air.local>
2023-03-08 21:27:50 +0900

    ens

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,12 @@
-import axios from "axios";
-import { useState } from "react";
-import { UserCard } from "./components/UserCard";
-import { User } from "./types/api/user";
 import "./App.css";
-import { UserProfile } from "./types/userProfile";
+import { UserCard } from "./components/UserCard";
+import { useAllUsers } from "./hooks/useAllUsers";
 
 export const App = () => {
-  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
-  const [loading, setloading] = useState(false);
-  const [error, setError] = useState(false);
+  // カスタムフック↓
+  const { getUsers, userProfiles, loading, error } = useAllUsers();
 
-  const onClickFetchUser = () => {
-    setloading(true);
-    setError(false);
-    axios
-      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => {
-        const data = res.data.map((user) => ({
-          id: user.id,
-          name: `${user.name}(${user.username})`,
-          email: user.email,
-          address: `${user.address.city}${user.address.suite}${user.address.street}`,
-        }));
-        setUserProfiles(data);
-      })
-      .catch(() => {
-        setError(true);
-      })
-      .finally(() => {
-        setloading(false);
-      });
-  };
+  const onClickFetchUser = () => getUsers();
 
   return (
     <div>
